84300feb8cbe932ea5fe6b057487d074442ae9bc
andre <andres.leon.orozco@est.una.ac.cr>
2022-09-13 10:30:15 -0600

    Initial commit

---
+++ src/ExamplePack/Vista.java
@@ -0,0 +1,94 @@
+package ExamplePack;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import javax.swing.event.InternalFrameEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+public class Vista{
+    private JFrame ventana;
+    private JMenu menu;
+    private JMenuItem item1;
+    private JMenuItem item2;
+    private JMenuItem item3;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel panel5;
+    private CardLayout card;
+    private JButton bot1;
+
+    public Vista(){
+        ventana = new JFrame("Laboratorio 2");
+        ventana.setSize(640,400);
+        card = new CardLayout();
+        ventana.setLayout(card);
+
+        menu = new JMenu("Menu");
+        JMenuBar barra = new JMenuBar();
+        barra.setSize(80,20);
+        item1 = new JMenuItem("Inicio");
+        item2 = new JMenuItem("Ejemplos");
+        item3 = new JMenuItem("Acerca de");
+        menu.add(item1);
+        menu.add(item2);
+        menu.add(item3);
+        barra.add(menu);
+
+        JLabel txt = new JLabel("Pulse el boton para salir");
+        item1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                card.show(ventana.getContentPane(),"Inicio");
+            }
+        });
+        panel1 = new JPanel();
+        panel1.setSize(50,50);
+        panel1.setLayout(new GridLayout(1,0));
+        panel1.add(txt);
+        bot1 = new JButton("Press");
+        bot1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               WindowEvent closeWindow = new WindowEvent(ventana,WindowEvent.WINDOW_CLOSING);
+               Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(closeWindow);
+            }
+        });
+        panel1.add(bot1);
+
+
+        panel3 = new JPanel();
+        panel3.setLayout(new GridLayout(1,0));
+        JLabel txtAD = new JLabel("Integrantes de Grupo");
+        txtAD.setForeground(Color.blue);
+        JLabel txtAD2 = new JLabel("Andres Leon"+" "+"Idanny Huertas"+" "+"Jennifer Quiros");
+        panel3.add(txtAD);
+        panel3.add(txtAD2);
+        item3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              card.show(ventana.getContentPane(),"AcercaDe");
+            }
+        });
+
+
+        JPanel main = new JPanel();
+        main.setLayout(new GridLayout(1,0));
+        JLabel txt2 = new JLabel("Laboratorio 2");
+        txt2.setSize(100,100);
+        main.add(txt2);
+        ventana.add(main,"main");
+        ventana.add(panel1,"Inicio");
+        ventana.add(panel3,"AcercaDe");
+        ventana.setJMenuBar(barra);
+        card.show(ventana.getContentPane(),"main");
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+}
---
+++ src/ExamplePack/source.java
@@ -0,0 +1,7 @@
+package ExamplePack;
+
+public class source {
+    public static void main(String[] args) {
+        Vista vis = new Vista();
+    }
+}
